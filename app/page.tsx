df40d64aa7e971e0eef07d7083010f4fd4d40c19
Rohitkumar A <rohitasare7@gmail.com>
2023-11-13 17:44:11 +0530

    initial static page with all information

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+          Welcome to the &nbsp;
+          <code className="font-mono font-bold">Friends Finder GG</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            Hosted  By{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -31,82 +31,67 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
+          src="/logo_svg.svg"
+          alt="FFGG Logo"
           width={180}
           height={37}
           priority
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="flex">
+        <div className="flex-1 mt-6">
+          <div className="mt-4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+              <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">13th Nov 2023</h5>
+            </a>
+            <p className="mb-2 font-normal text-gray-900 dark:text-gray-400">Please find below updates :</p>
+            <ul className="w-full text-sm space-y-1 text-gray-800 list-disc list-inside dark:text-gray-400">
+              <li>
+                Basic framework - building the site on my local machine with Laravel + VueJS + some APIs.
+              </li>
+              <li>
+                Added Newsletter option using Brevo to keep everyone in mailing list updated.
+              </li>
+              <li>
+                Current site is static and hosted on Vercel with NextJS but we will move to Laravel + VueJS once the MVP development is complete.
+              </li>
+              <li>
+                I am figuring out the APIs from different networks like PlayStation, Xbox, Steam etc.
+              </li>
+            </ul>
+            <div className="p-4 mt-4 border text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+              <span className="font-medium"> <a className='text-blue-700 font-medium' href='https://discord.gg/qjmXdBSWcw' target='_blank'>Join Discord</a></span> to contribute ideas, discuss features and help if you know APIs/Web Development. You can also subscribe to newsletter to stay updated via email.
+            </div>
+            <div className="p-4 mt-3 border text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+              <span className="font-medium"><a className=' font-medium' href='https://github.com/rohitasare7/friends-finder-gg/' target='_blank'>GitHub Repo</a></span> I am keeping the development open source so anyone can contribute, need to work on contribution strategy/devops stuff.
+            </div>
+            <div className="p-4 mt-3 border text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
+              <span className="font-medium">What we need : </span> A Hosting Server, I am figuring out how to host everything for free on Vercel platform first and later we can move on to proper Server. This is future scope but I am keeping the scalability in my mind while developing the whole architecture.
+            </div>
+            <div className='mt-4'>
+              <label className="block mb-3 text-sm font-medium text-gray-900 dark:text-white">Shortened link for Sharing (Select and then Copy)</label>
+              <input type="text" id="large-input" value={'http://gg.gg/friendsfinder'} className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            </div>
+          </div>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="flex-1">
+          <div style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: '100%', borderRadius: '10px' }}>
+            <iframe
+              width="540"
+              height="755"
+              src="https://bf93fd43.sibforms.com/serve/MUIFAOUBQarhr2cfDr7Eo4ZjyXNcgN3qYPSss8VMVj6pRz3stWOButeDmgwjfDaOKMOaHh52NJAOaJB7cQyQWU_C54A1-IlCj4TSUdXZcQ1BBl-X_E8f7ZXrg5cA30C7G4eszvx-vlFIXYT-6aWvJcnpItUPjUJE-GoQmp4yAd8uA48NJonV4fnfmXFbR-Hnp9XvTR8g81GVHhvh"
+              frameBorder="0"
+              scrolling="auto"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="">
+        Site Designed with Love, by a <span className='font-medium'>Gamer for Gamers</span>
       </div>
     </main>
   )
